eefafb84e995518bb851ff0c0e2389a08e56587b
Tahmidul Azom Sany <tasanyphy01773@gmail.com>
2023-09-06 15:26:04 -0400

    php file

---
+++ Blog_V2/colab_html/navbar.php
@@ -0,0 +1,34 @@
+<!-- navbar.php -->
+<nav id="navbar" class="navbar navbar-light navbar-expand-sm fixed-top">
+    <div class="container">
+        <a class="navbar-brand title font-weight-lighter" href="/"><span class="font-weight-bold">Abdullah Al Fahad, Ph.D.</span></a>
+        <button class="navbar-toggler collapsed ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar top-bar"></span>
+            <span class="icon-bar middle-bar"></span>
+            <span class="icon-bar bottom-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse text-right" id="navbarNav">
+            <ul class="navbar-nav ml-auto flex-nowrap">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/research/">Research<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/blog/">Blog</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/ClimBuntu/">ClimBuntu<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="toggle-container">
+                    <button id="light-toggle" title="Change theme">
+                        <i class="fas fa-moon"></i>
+                        <i class="fas fa-sun"></i>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
