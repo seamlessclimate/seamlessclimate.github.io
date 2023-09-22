98fed57d03fd0bcb8af85e8f51d2386e4618de30
Tashin <79139145+tashin42@users.noreply.github.com>
2023-09-22 11:35:22 +0600

    Update users.js

--- blog/users.js
+++ blog/users.js
@@ -1,13 +1,13 @@
 // Initialize Firebase with your configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC2L31O2VtvaXWe-ktH77iMagARxdNqB88",
-  authDomain: "climpy-e9dab.firebaseapp.com",
-  projectId: "climpy-e9dab",
-  storageBucket: "climpy-e9dab.appspot.com",
-  messagingSenderId: "194247994975",
-  appId: "1:194247994975:web:422d737a1bd25c0a4335ea",
-  measurementId: "G-0R00GJTLX5"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC2L31O2VtvaXWe-ktH77iMagARxdNqB88",
+//   authDomain: "climpy-e9dab.firebaseapp.com",
+//   projectId: "climpy-e9dab",
+//   storageBucket: "climpy-e9dab.appspot.com",
+//   messagingSenderId: "194247994975",
+//   appId: "1:194247994975:web:422d737a1bd25c0a4335ea",
+//   measurementId: "G-0R00GJTLX5"
+// };
 
 firebase.initializeApp(firebaseConfig);
 
