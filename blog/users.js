9c4303ad4d88d890db427e3784c0e0eebce3061b
Tashin <79139145+tashin42@users.noreply.github.com>
2023-09-24 07:44:33 +0600

    Update users.js

--- blog/users.js
+++ blog/users.js
@@ -1,16 +1,3 @@
-// Initialize Firebase with your configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC2L31O2VtvaXWe-ktH77iMagARxdNqB88",
-//   authDomain: "climpy-e9dab.firebaseapp.com",
-//   projectId: "climpy-e9dab",
-//   storageBucket: "climpy-e9dab.appspot.com",
-//   messagingSenderId: "194247994975",
-//   appId: "1:194247994975:web:422d737a1bd25c0a4335ea",
-//   measurementId: "G-0R00GJTLX5"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-
 // Reference to the "users" node in your Firebase Realtime Database
 const usersRef = firebase.database().ref('users');
 
@@ -37,6 +24,6 @@ usersRef.once('value', (snapshot) => {
   const totalUsersElement = document.getElementById('totalUsers');
   const uniqueInstitutionsElement = document.getElementById('uniqueInstitutions');
 
-  totalUsersElement.textContent = "Total Users: " + totalUsers;
-  uniqueInstitutionsElement.textContent = "Institutions: " + uniqueInstitutions.size;
+  totalUsersElement.textContent = totalUsers;
+  uniqueInstitutionsElement.textContent = uniqueInstitutions.size;
 });
